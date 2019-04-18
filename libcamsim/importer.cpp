baaada079981376f34015146a92e8d637b33a6c4
Martin Lambers <marlam@marlam.de>
2019-04-18 10:39:58 +0200

    Enable mipmaps for imported textures.

--- libcamsim/importer.cpp
+++ libcamsim/importer.cpp
@@ -190,7 +190,7 @@ static unsigned int createTex(
                     GL_RGBA, GL_UNSIGNED_BYTE, img.constBits());
             gl->glGenerateMipmap(GL_TEXTURE_2D);
             gl->glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-            gl->glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+            gl->glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
             switch (mapmode[0]) {
             case aiTextureMapMode_Wrap:
                 gl->glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
